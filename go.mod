f14fb46f5ce6cbb4b7d82f04255511b0d4d3b93a
Abhishek Kumar <abhirajabhi312@gmail.com>
2025-04-12 15:33:53 +0530

    added coordinator server start,submittask,grpcServer startup code

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.24.2
 require (
 	github.com/jackc/pgtype v1.14.0
 	github.com/jackc/pgx/v4 v4.18.3
+	google.golang.org/grpc v1.71.1
 )
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -16,6 +18,10 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.20.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
 )
--- pkg/coordinator/coordinator.go
+++ pkg/coordinator/coordinator.go
@@ -2,10 +2,17 @@ package coordinator
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"log/slog"
 	"net"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
 )
@@ -22,7 +29,7 @@ type CoordinatorServer struct {
 	serverPort          string
 	listner             net.Listener
 	grpcServer          *grpc.Server
-	WorkerPool          map[uint32]*workerInfo
+	WorkerPool          map[uint32]*WorkerInfo
 	WorkerPoolMutex     sync.Mutex
 	WorkerPoolKeys      []uint32
 	WorkerPoolKeysMutex sync.RWMutex
@@ -57,3 +64,87 @@ func NewServer(port string, dbconnStr string) *CoordinatorServer {
 		cancel:             cancel,
 	}
 }
+
+func (c *CoordinatorServer) Start() error {
+	var err error
+
+	// start WorkerPool
+	// Workers can self-register just by sending a heartbeat. The server stays updated on which workers are alive.
+	//Supports automatic recovery if a worker restarts and reconnects.
+
+	go c.manageWorkerPool() // removes the inactive workers from the pool
+
+	// start the grpcServer
+	if err = c.startGRPCServer(); err != nil {
+		return fmt.Errorf("gRPC server error,coudn't start the server %w", err)
+	}
+	// connect to dbPool
+	s.dbPool, err = common.ConnectToDB(c.ctx, c.dbString)
+	if err != nil {
+		return err
+	}
+
+	// scan the db at regular interval
+	go c.scanDB()
+	// returnt the await shutdown function if the program stops this also
+	return c.awaitShutDown()
+}
+
+func (c *CoordinatorServer) awaitShutDown() error {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+
+	return c.Stop()
+}
+
+func (c *CoordinatorServer) Stop() error {}
+
+// startGRPCServer function starts the gRPC server
+
+func (c *CoordinatorServer) startGRPCServer() error {
+	var err error
+	c.listner, err = net.Listen("tcp", c.serverPort)
+	if err != nil {
+		return err
+	}
+
+	slog.Info("starting gRPC server", fmt.Sprintf("Addr"), c.serverPort)
+	c.grpcServer = grpc.NewServer()
+	pb.RegisterCoordinatorServiceServer(c.grpcServer, c)
+
+	go func() {
+		if err := c.grpcServer.Serve(c.listner); err != nil {
+			log.Fatal("gRPC server failed to start: %v", err)
+		}
+	}()
+
+	return nil
+
+}
+
+// SubmitTask takes in the task from porotobuf defined spec and calls the submittaskWorker()
+func (c *CoordinatorServer) SubmitTask(ctx context.Context, in *pb.ClientTaskRequest) (*pb.ClientTaskResponse, error) {
+	// get the data from ClientTaskRequest
+	data := in.GetClientData()
+
+	// Attaches the uuid to each new task randomly
+	taskID := uuid.New().String()
+
+	// construct the task struct to send the response to client
+	task := &pb.TaskRequest{
+		TaskId: taskID,
+		Data:   data,
+	}
+	if err := c.dispatchTasktoWorker(task); err != nil {
+		return nil, err
+	}
+
+	return &pb.ClientTaskResponse{
+		Message: "Task submitted successfully",
+		TaskId:  taskID,
+	}, nil
+
+}
+func (c *CoordinatorServer) dispatchTasktoWorker(task *pb.TaskRequest) error {}
